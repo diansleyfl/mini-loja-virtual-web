e21cb9af9d39d1f0ae063a078ecb18822d0ae2b5
Diansley Fernandes de Lima <diansley2@hotmail.com>
2021-10-07 16:51:10 -0300

    aplicação responsivo

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,37 +3,43 @@ const items = [
         id: 0,
         nome: 'Camiseta Vasco Templária',
         img: 'camisa01.jpg',
-        quantidade: 0
+        quantidade: 0,
+        preco: "49,90"
     },
     {
         id: 1,
         nome: 'Camiseta Vasco LGBT Branca',
         img: 'camisa02.jpg',
-        quantidade: 0
+        quantidade: 0,
+        preco: "179,90"
     },
     {
         id: 2,
         nome: 'Camiseta Vasco Preta',
         img: 'camisa03.jpg',
-        quantidade: 0
+        quantidade: 0,
+        preco: "199,90"
     },
     {
         id: 3,
         nome: 'Camiseta Vasco Branca',
         img: 'camisa04.jpg',
-        quantidade: 0
+        quantidade: 0,
+        preco: "199,90"
     },
     {
         id: 4,
         nome: 'Camiseta Vasco Goleiro III',
         img: 'camisa05.jpg',
-        quantidade: 0
+        quantidade: 0,
+        preco: "169,90"
     },
     {
         id: 5,
         nome: 'Camiseta Vasco LGBT Preta',
         img: 'camisa06.jpg',
-        quantidade: 0
+        quantidade: 0,
+        preco: "179,90"
     },
 ]
 
@@ -45,7 +51,8 @@ inicializarLoja = () => {
        
        <div class="produto-single">
             <img src="./assets/img/`+val.img+`" />
-            <p>`+val.nome+`</p>
+            <p class="descricao">`+val.nome+`</p>
+            <p class="preco">`+val.preco+`</p>
             <a key="`+val.id+`" href="#">Adicionar ao carrinho<a/>
         </div>
     
@@ -65,7 +72,6 @@ atualizarCarrinho = () => {
        
        <p>`+val.nome+` | quantidade: `+val.quantidade+`</p>
        <hr>
-
        
        `;
         }
